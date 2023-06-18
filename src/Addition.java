cb2bdee45ff637004acd3f7b4d5c4de664aed023
rajesh786 <pakkalarajesh786@gmail.com>
2023-06-18 21:31:44 +0530

    Additon

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Addition{
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        int a=20;
+        int b=40;
+        int c=a+b;
+        System.out.print(c);
+
+
+
+    }
+}

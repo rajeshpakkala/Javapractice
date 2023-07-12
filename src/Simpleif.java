ab24e279fce651eff371f7173aeb6c5ec21c5ec8
rajeshpakkala <pakkalarajesh786@gmail.com>
2023-07-12 13:57:58 +0530

    changes in if

--- src/Simpleif.java
+++ src/Simpleif.java
@@ -2,6 +2,7 @@ public class Simpleif {
     public static void main(String[]args){
         int a=22;
         int b=48;
+        int c=22;
         if(a<b){
             System.out.println("b value is larger");
         }

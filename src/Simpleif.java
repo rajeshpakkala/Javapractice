e328c91f4775c820557fc83fa2b633b1ff49f0d6
rajeshpakkala <pakkalarajesh786@gmail.com>
2023-07-12 13:54:07 +0530

    if condition

---
+++ src/Simpleif.java
@@ -0,0 +1,9 @@
+public class Simpleif {
+    public static void main(String[]args){
+        int a=22;
+        int b=48;
+        if(a<b){
+            System.out.println("b value is larger");
+        }
+    }
+}
